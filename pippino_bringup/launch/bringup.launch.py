f4328a235fba807c4a9d8089ed3f093e99f19483
Michele De Marchi <michele.demarchi.85@gmail.com>
2023-11-05 15:32:24 +0100

    fixed bringup scripts and nav2 params. next trying to add controller settings for full coverage path optimization

--- pippino_bringup/launch/bringup.launch.py
+++ pippino_bringup/launch/bringup.launch.py
@@ -1,5 +1,8 @@
 import launch
-from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition
+
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 import launch_ros
 import os
@@ -15,20 +18,33 @@ def generate_launch_description():
     display_dir = launch_ros.substitutions.FindPackageShare(package='pippino_display').find('pippino_display')
     display_launch_dir = os.path.join(display_dir, 'launch')
 
+    display = LaunchConfiguration('display')
+
     # Launch pippino description (tf, 3d model, etc.)
     start_description = launch.actions.IncludeLaunchDescription(
         PythonLaunchDescriptionSource(os.path.join(description_launch_dir, 'description.launch.py'))
     )
-    start_navigation = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(navigation_launch_dir, 'navigation.launch.py')),
+    # start_navigation = launch.actions.IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(os.path.join(navigation_launch_dir, 'navigation.launch.py')),
+    #     launch_arguments = {'autostart': 'True'}.items()
+    # )
+
+    start_navigation_with_slam = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(os.path.join(navigation_launch_dir, 'bringup.launch.py')),
         launch_arguments = {'autostart': 'True'}.items()
     )
 
-    start_slam = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(navigation_launch_dir, 'slam.launch.py'))
-    )
+    # start_slam = launch.actions.IncludeLaunchDescription(
+    #     PythonLaunchDescriptionSource(os.path.join(navigation_launch_dir, 'slam.launch.py'))
+    # )
+    declare_display_enable = DeclareLaunchArgument(
+        'display',
+        default_value='true',
+        description='Whether to launch display (rviz).')
+
     start_display = launch.actions.IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(os.path.join(display_launch_dir, 'display.launch.py'))
+        PythonLaunchDescriptionSource(os.path.join(display_launch_dir, 'display.launch.py')),
+        condition=IfCondition(display)
     )
 
     rosbridge_server_node = launch_ros.actions.Node(
@@ -54,6 +70,7 @@ def generate_launch_description():
         executable='aruco_detection_exe',
         name='aruco_detection_service',
         output='screen',
+        # output={'both': 'log'},
         parameters=[{
             'use_sim_time': False,
             'image_topic': '/D455/color/image_raw',
@@ -65,12 +82,21 @@ def generate_launch_description():
         package='autodock_action_server',
         executable='autodock_action_server_exe',
         output='screen'
+        # output={'both': 'log'},
     )
 
     autodock_action_client_node = launch_ros.actions.Node(
         package='autodock_action_client',
         executable='autodock_action_client_exe',
         output='screen'
+        # output={'both': 'log'},
+    )
+
+    explorer_action_client_node = launch_ros.actions.Node(
+        package='explorer_action_client',
+        executable='explorer_action_client_exe',
+        output='screen'
+        # output={'both': 'log'},
     )
 
     start_explorer_wanderer_server = launch.actions.IncludeLaunchDescription(
@@ -80,24 +106,28 @@ def generate_launch_description():
     start_node_webserver = launch.actions.ExecuteProcess(
         cmd=['npm', 'start', '--prefix=~/ros-ui-react/example/']
     )
-    # start_fastdds_server = launch.actions.ExecuteProcess(
-    #     cmd=['/home/michele/fastdds_server.sh']
-    # )
 
     return launch.LaunchDescription([
         # Start FastDDS server
         # start_fastdds_server
-
+        declare_display_enable,
         ## start_description,
-        launch.actions.TimerAction(period=8.0, actions=[start_navigation]),
-        launch.actions.TimerAction(period=2.0, actions=[start_slam]),
+        ## launch.actions.TimerAction(period=8.0, actions=[start_navigation]),
+        ##launch.actions.TimerAction(period=2.0, actions=[start_slam]),
+        launch.actions.TimerAction(period=2.0, actions=[start_navigation_with_slam]),
         launch.actions.TimerAction(period=1.0, actions=[start_display]),
-        ## rosbridge_server_node,
+        rosbridge_server_node,
         ## teleop_twist_joy_node,
-        # start_explorer_wanderer_server,
         aruco_detection_node,
+        
+        # Find yourself and map room
+        start_explorer_wanderer_server,
+        explorer_action_client_node,
+
+        # Autodocking
         autodock_action_server_node,
         autodock_action_client_node,
+        
         start_node_webserver,
         ## web_video_server_node
     ])
